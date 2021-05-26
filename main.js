b670ff854bcc76e3ccd26b4f746d92e913a6e95b
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2021-05-26 20:21:59 +0600

    all continents can be seen in UI

--- main.js
+++ main.js
@@ -1,16 +1,51 @@
-fetch('https://countries.trevorblades.com/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-        query: `
+const continentSelect = document.getElementById("continent-select")
+
+queryFetch(`
         query{
             continents{
                 name
+                code
             }
         }
-    `
-    })
-}).then(response => response.json())
+    `)
     .then(data => {
         console.log(data.data.continents);
+        data.data.continents.forEach(continent => {
+            const option = document.createElement('option')
+            option.value = continent.code;
+            option.innerText = continent.name;
+            continentSelect.append(option)
+        })
     })
+
+continentSelect.addEventListener("change", async e => {
+    const continentCode = e.target.value
+    const countries = await getContinentCountries(continentCode)
+})
+
+function getContinentCountries(continentCode) {
+    return queryFetch(`
+        query getCountries($code: String){
+            continent(code: $code){
+                countries{
+                    name
+                }
+            }
+        }
+        `, { code: continentCode }).then(data => {
+        console.log(data);
+    })
+}
+
+
+
+function queryFetch(query, variables) {
+    return fetch('https://countries.trevorblades.com/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })
+    }).then(response => response.json())
+}
